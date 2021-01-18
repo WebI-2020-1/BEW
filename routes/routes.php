55679781b8ebeff06cb96c5954f2cc6bc933a971
claudio_dev <claudio@focomultimidia.com>
2021-01-18 09:55:50 -0300

    Cria nova rota para register

--- routes/routes.php
+++ routes/routes.php
@@ -4,6 +4,7 @@
     Route::get('/login', 'LoginController@index');
     Route::post('/login/log-into', 'LoginController@store');
     Route::get('/register', 'RegisterController@index');
+    Route::post('/register/into', 'RegisterController@store');
     
     Route::run();
 ?>
--- src/Views/RegisterView.php
+++ src/Views/RegisterView.php
@@ -8,10 +8,10 @@
 <title> Registro de Usuário </title>
 </head>
 <body>
-<form method="POST" action="/register">
+<form method="POST" action="/register/into">
 <label>Login:</label><input type="text" name="login" id="login"><br>
 <label>Senha:</label><input type="password" name="senha" id="senha"><br>
-<label>Confirmar senha:</label><input type="password" name="senhas" id="senhas"><br>
+<label>Confirmar senha:</label><input type="password" name="confirmar-senha" id="confirmar-senha"><br>
 <button type="submit">Cadastrar</button>
 <a href="/login">Logue-se</a>
 </form>
