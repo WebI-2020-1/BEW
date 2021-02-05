dbceecd69216941aadfc3c369b29e6a5fb70871d
Mateus Gomes <mateus.gs1999@gmail.com>
2021-02-04 23:03:25 -0300

    adicionado classes, icones e css

--- src/Views/LoginView.php
+++ src/Views/LoginView.php
@@ -1,6 +1,6 @@
 <?php
     class LoginView{
-        
+
     }
 ?>
 <html lang='pt-BR'>
@@ -8,19 +8,32 @@
   <title> Login de Usuário </title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../public/css/login.css">
 </head>
+
+<script src="https://unpkg.com/feather-icons"></script>
+
 <body>
-<form method="POST" action="login/log-into">
-<label>Login:</label><input type="text" name="login" id="login"><br>
-<label>Senha:</label><input type="password" name="password" id="password"><br>
-<button type="submit">Entrar</button>
-<a href="/register">Cadastre-se</a>
-<h1>
-    <?php 
-        echo $_SESSION['message']; 
-        unset($_SESSION['message']);
-    ?>
-</h1>
-</form>
+  <img src="../../public/img/logoAbelha.png" alt="logo da Abelha">
+  <h1>BEW</h1>
+  <form method="POST" action="login/log-into">
+    <i data-feather="user"></i><label>Login:</label>
+    <input type="text" name="login" id="login" class="blocologin"><br>
+    <i data-feather="lock"></i><label>Senha:</label>
+    <input type="password" name="password" id="password" class="blocologin"><br>
+    <button type="submit" class="blocologin btnEnter">Entrar</button>
+    <a href="/register">Esqueci minha senha</a> <i data-feather="arrow-right"></i>
+    <h1>
+        <?php
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        ?>
+    </h1>
+  </form>
 </body>
+
+<script>
+  feather.replace()
+</script>
+
 </html>
