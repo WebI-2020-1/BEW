ff27529da0124cbb6be89e6dbb4518f4fc2733df
Mateus Gomes <mateus.gs1999@gmail.com>
2021-02-04 23:15:06 -0300

    adiciona ids

--- src/Views/LoginView.php
+++ src/Views/LoginView.php
@@ -14,22 +14,24 @@
 <script src="https://unpkg.com/feather-icons"></script>
 
 <body>
-  <img src="../../public/img/logoAbelha.png" alt="logo da Abelha">
-  <h1>BEW</h1>
-  <form method="POST" action="login/log-into">
-    <i data-feather="user"></i><label>Login:</label>
-    <input type="text" name="login" id="login" class="blocologin"><br>
-    <i data-feather="lock"></i><label>Senha:</label>
-    <input type="password" name="password" id="password" class="blocologin"><br>
-    <button type="submit" class="blocologin btnEnter">Entrar</button>
-    <a href="/register">Esqueci minha senha</a> <i data-feather="arrow-right"></i>
-    <h1>
-        <?php
-            echo $_SESSION['message'];
-            unset($_SESSION['message']);
-        ?>
-    </h1>
-  </form>
+  <div class="container">
+    <img src="../../public/img/logoAbelha.png" alt="logo da Abelha" id="logo">
+    <h1 id="titulo">BEW</h1>
+    <form method="POST" action="login/log-into">
+      <i data-feather="user"></i><label>Login:</label>
+      <input type="text" name="login" id="login" class="blocologin"><br>
+      <i data-feather="lock"></i><label>Senha:</label>
+      <input type="password" name="password" id="password" class="blocologin"><br>
+      <button type="submit" class="blocologin btnEnter">Entrar</button><br>
+      <a href="/register">Esqueci minha senha</a> <i data-feather="arrow-right"></i>
+      <h1>
+          <?php
+              echo $_SESSION['message'];
+              unset($_SESSION['message']);
+          ?>
+      </h1>
+    </form>
+  </div>
 </body>
 
 <script>
