d2872a9e638440736ecfb6f492a255dd6d689077
Mateus Gomes <mateus.gs1999@gmail.com>
2021-02-04 21:40:06 -0300

    adicionado linguagem e a tag metas

--- src/Views/LoginView.php
+++ src/Views/LoginView.php
@@ -5,17 +5,19 @@
         }
     }
 ?>
-<html>
+<html lang='pt-BR'>
 <head>
-<title> Login de Usuário </title>
+  <title> Login de Usuário </title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<form method="POST" action="login/log-into">
-<label>Login:</label><input type="text" name="login" id="login"><br>
-<label>Senha:</label><input type="password" name="senha" id="senha"><br>
-<button type="submit">Entrar</button>
-<a href="/register">Cadastre-se</a>
-<h1><?php print_r($params) ?></h1>
-</form>
+  <form method="POST" action="login/log-into">
+    <label>Login:</label><input type="text" name="login" id="login"><br>
+    <label>Senha:</label><input type="password" name="senha" id="senha"><br>
+    <button type="submit">Entrar</button>
+    <a href="/register">Cadastre-se</a>
+    <h1><?php print_r($params) ?></h1>
+  </form>
 </body>
 </html>
