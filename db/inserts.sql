a757903dcd511ba53b31c63cde950a307155e136
Mateus Gomes <mateus.gs1999@gmail.com>
2021-02-04 21:02:26 -0300

    correção de sintaxe

--- db/inserts.sql
+++ db/inserts.sql
@@ -7,7 +7,7 @@ INSERT INTO Produto (nome, unidade, valorVenda, codigoBarras, idCategoria) VALUE
     ('Mel', '1kg', 23.50, 'mel2456', 2),
     ('Abelha Orgânica', '10un', 50.00, 'bee50u', 2);
 
-INSERT INTO funcionario (nome, nivelAcesso, cpf, endereco, telefone, dataNascimento, usuario, senha, email) VALUES
+INSERT INTO Funcionario (nome, nivelAcesso, cpf, endereco, telefone, dataNascimento, usuario, senha, email) VALUES
     ('Natasha', 0, '123.456.789-00', 'Rua A', '(12) 34567-8900', '2000-02-18', 'Nat', '202cb962ac59075b964b07152d234b70', 'natasha@buonasera.com'),
     ('Katuxa', 1, '012.345.678-90', 'Rua B', '(01) 23456-7890', '1998-08-02', 'Kat', '250cf8b51c773f3f8dc8b4be867a9a02', 'katuxa@buonasera.com');
 
@@ -23,12 +23,12 @@ INSERT INTO FormaPagamento (descricao) VALUES
 INSERT INTO Fornecedor (nome, cnpj, endereco, telefone, email) VALUES
     ('Lucas', '05.103.548/0001-80', 'Rua Jesus', '(12) 99255-4242', 'lucas@lapa.com'),
     ('Uelio', '17.733.974/0001-87', 'Rua C', '(55) 95641-5423', 'uelio@vet.com');
-    
+
 INSERT INTO Compra (notaFiscal, idFornecedor, idFuncionario) VALUES
     ('123', 1, 2),
     ('456', 2, 1);
 
-INSERT INTO produtocompra (valorUnitario, quantidade, idProduto, idCompra) VALUES
+INSERT INTO ProdutoCompra (valorUnitario, quantidade, idProduto, idCompra) VALUES
     (2.00, 80, 1, 1),
     (2.50, 20, 1, 2),
     (12.50, 100, 2, 2),
