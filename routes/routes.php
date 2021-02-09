a7c5e356e2c6efa12b21f4bcb3e5ea766c522902
Claudio Henrique <HacskBoy@hotmail.com>
2021-02-09 11:33:11 -0300

    Adiciona rotas de produto e categoria

--- routes/routes.php
+++ routes/routes.php
@@ -1,15 +1,27 @@
 <?php 
     require_once('app.php');
-    
+
+    // Rotas de login
     Route::get('/login', 'LoginController@index');
     Route::post('/login/log-into', 'LoginController@store');
 
+    // Rotas de registro
     Route::get('/register', 'RegisterController@index');
     Route::post('/register/into', 'RegisterController@store');
-    
+
+    // Rotas de dasboard
     Route::get('/dashboard', 'DashboardController@index');
 
+    // Rotas de logout
     Route::get('/logout', 'LogoutController@logout');
     
+    // Rotas de produto
+    Route::get('/add/product', 'ProductController@index'); 
+
+    // Rotas de categoria
+    Route::get('/add/category', 'CategoryController@index');
+    Route::post('/adding/category', 'CategoryController@store'); 
+
+
     Route::run();
 ?>
