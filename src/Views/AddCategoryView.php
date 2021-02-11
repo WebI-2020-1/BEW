ee4b921bcacd1ac90514095ad26372946f7e822c
Claudio Henrique <HacskBoy@hotmail.com>
2021-02-11 08:05:19 -0300

    Altera view de categoria

--- src/Views/AddCategoryView.php
+++ src/Views/AddCategoryView.php
@@ -4,7 +4,7 @@
     }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,10 +17,10 @@
         <button type="submit">Entrar</button>
     </form>
     <h1>
-    <?php 
-        echo $_SESSION['message']; 
-        unset($_SESSION['message']);
-    ?>
-</h1>
+        <?php 
+            echo $_SESSION['message']; 
+            unset($_SESSION['message']);
+        ?>
+    </h1>
 </body>
 </html>
