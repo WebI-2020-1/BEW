87f6783ef22bcd6128ead163e15d4eafda7821ee
Mateus Gomes <mateus.gs1999@gmail.com>
2021-02-16 15:47:21 -0300

    página no erro 404 estilizada

--- src/Views/404.php
+++ src/Views/404.php
@@ -1,1 +1,14 @@
-<h1>Pagina não encontrada :)</h1>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Erro 404</title>
+  <link rel="stylesheet" href="../../public/css/404.css">
+</head>
+<body>
+  <h1>404</h1>
+  <h2>Página não encontrada</h2>
+</body>
+</html>
