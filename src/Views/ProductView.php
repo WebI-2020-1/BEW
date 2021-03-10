ffbbc181d4059ccafa5fc37c8ba88da99ff55f89
Mateus Gomes <mateus.gs1999@gmail.com>
2021-03-09 21:06:07 -0300

    adicionado header e modal

--- src/Views/ProductView.php
+++ src/Views/ProductView.php
@@ -2,20 +2,35 @@
     class ProductView{
         public function __construct($params){ ?>
             <!DOCTYPE html>
-            <html lang="en">
+            <html lang="pt-BR">
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                <style>
-                    *{
-                        color:#19bf72;
-                    }
-                </style>
+                <link rel="stylesheet" href="/public/css/product.css">
+                <title>Produtos</title>
             </head>
+
+            <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+            <script src="https://unpkg.com/feather-icons"></script>
+
             <body>
-            <a href="/add/product">Adicionar produtos</a>
-                <table>
+            <?php include "components/Sidebar.php" ?>
+            <main class="wide">
+            <header>
+                <i class="menu-toggle" data-feather="menu"></i>
+                <div class="header-conteudo">
+                    <h1>PRODUTOS</h1>
+                    <div class="botoesDireito">
+                        <a href="/add/product" class="btnAdd">Adicionar produto<i data-feather="plus"></i></a>
+                        <form action="" class="pesquisar">
+                            <input type="search" name="pesquisar" id="" placeholder="Pesquisar na tabela">
+                            <button type="submit"><i data-feather="search"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </header>
+            <div class="content">
+                <table class="tabela-consulta">
                     <thead>
                         <tr>
                             <td>ID</td>
@@ -24,7 +39,6 @@
                             <td>Quantidade</td>
                             <td>Valor de compra</td>
                             <td>Valor de venda</td>
-                            <td>Codigo de barras</td>
                             <td>Ação</td>
                         </tr>
                     </thead>
@@ -37,22 +51,37 @@
                                 <td><?php echo $product['quantidade']; ?></td>
                                 <td><?php echo $product['valorCompra']; ?></td>
                                 <td><?php echo $product['valorVenda']; ?></td>
-                                <td><?php echo $product['codigoBarras']; ?></td>
-                                <td>
-                                    <a href="/edit/product&id=<?php echo $product['id']; ?>">Editar</a>
-                                    <a href="/delete/product&id=<?php echo $product['id']; ?>">Deletar</a>
+                                <td><button type="button" class="abrir-modal" onclick="consultarProduto(<?php echo $product['id']; ?>)">
+                                    <i data-feather="search"></i></button>
                                 </td>
                             </tr>
                         <?php } ?>
+                        <div class="modal disabled">
+                            <div class="produto">
+                                <button type="button" class="fechar-modal">
+                                    <i data-feather="x"></i>
+                                </button>
+                                <div class="conteudoProduto"></div>
+                                <a href="/edit/product&id=<?php echo $product['id']; ?>">Editar</a>
+                                <a href="/delete/product&id=<?php echo $product['id']; ?>">Deletar</a>
+                                <a href="" class="btnEditar">Editar<i data-feather="edit"></i></a>
+                            </div>
+                        </div>
                     </tbody>
                 </table>
+                </div>
                 <h1>
                     <?php
                         echo $_SESSION['message'];
                         unset($_SESSION['message']);
                     ?>
                 </h1>
+            </main>
             </body>
+
+            <script src="/public/js/global.js"></script>
+            <script src="/public/js/ProductFunctions.js"></script>
+
             </html>
         <?php }
     }
