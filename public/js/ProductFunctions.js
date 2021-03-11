64223197a730b35476dc9eae87e35eddfe0213c8
Mateus Gomes <mateus.gs1999@gmail.com>
2021-03-11 11:47:36 -0300

    adicinado metodos do produto

--- public/js/ProductFunctions.js
+++ public/js/ProductFunctions.js
@@ -13,3 +13,70 @@ fecharModalElements.forEach((elemento) => { elemento.addEventListener("click", (
 /* --------------- PRODUTO -------------- */
 const modalProduto = document.querySelector(".conteudoProduto");
 
+const consultarProduto = (id) => {
+  axios
+    .post(`${host}/getProduct`,
+        (`id=${id}`),
+        {
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded",
+            },
+        }
+    )
+    .then((response) => {
+      const produto = response.data;
+      modalProduto.innerHTML =
+        `<h2>${produto.nome}</h2>
+        <table class="info">
+        <tr>
+          <th>ID</th>
+          <td>${produto.id}</td>
+        </tr>
+        <tr>
+          <th>Unidade</th>
+          <td>${produto.unidade}</td>
+        </tr>
+        <tr>
+          <th>Quantidade</th>
+          <td>${produto.quantidade}</td>
+        </tr>
+        <tr>
+          <th>Categoria</th>
+          <td>${produto.nomeCategoria}</td>
+        </tr>
+        <tr>
+          <th>Valor de Venda</th>
+          <td>${produto.valorVenda}</td>
+        </tr>
+        <tr>
+          <th>Valor de Compra</th>
+          <td>${produto.valorCompra}</td>
+        </tr>
+        <tr>
+          <th>Código de Barras</th>
+          <td>${produto.codigoBarras}</td>
+        </tr>
+        </table>`;
+    })
+    .catch((err) => console.log(err));
+}
+
+const limparProduto = () => {
+  modalProduto.innerHTML = "";
+}
+
+const filtrarProduto = () => {
+  const tr = document.querySelectorAll(".tabela-consulta tbody tr");
+  const filter = document.getElementById("input").value.toUpperCase();
+  for (let i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
