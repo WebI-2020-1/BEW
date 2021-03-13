051dcae42425ac30097af95d8364fd5256f6c55c
Claudio Henrique <claudio@focomultimidia.com>
2021-03-13 09:47:48 -0300

    Debugando variaveis de ambiente

--- src/database/DB.php
+++ src/database/DB.php
@@ -6,6 +6,12 @@ class DB
   public function __construct()
   {
     $env = parse_ini_file('env.ini');
+    if(empty($env) || !isset($env)){
+      $env['DB_HOST'] = getenv('DB_HOST');
+      $env['DB_DATABASE'] = getenv('DB_DATABASE');
+      $env['DB_USERNAME'] = getenv('DB_USERNAME');
+      $env['DB_PASSWORD'] = getenv('DB_PASSWORD');
+    }
     self::$db = new PDO('mysql:host=' . $env['DB_HOST'] . ';dbname=' . $env['DB_DATABASE'], $env['DB_USERNAME'], $env['DB_PASSWORD'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
     self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
