82483ce90ad6326787c2edb64d0cda8a7a37c71d
Marcio Samuel <marciosamuel12@outlook.com>
2021-03-18 00:16:08 -0300

    ajuste de visibilidade da sidebar

--- src/Views/AddProviderView.php
+++ src/Views/AddProviderView.php
@@ -21,9 +21,9 @@ class AddProviderView
 
         <body>
             <?php include "components/Sidebar.php" ?>
-            <main class="wide">
+            <main>
                 <header>
-                    <i class="menu-toggle" data-feather="menu"></i>
+                    <i class="menu-toggle disabled" data-feather="menu"></i>
                     <h1>ADICIONAR FORNECEDOR</h1>
                 </header>
 
